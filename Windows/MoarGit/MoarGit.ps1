920b28a5b2aa35ac0e3f3305a6f87d3f79422ae0
Hector Pedraza <ettodrzz@gmail.com>
2023-10-22 09:56:14 -0600

    Add MoarGit.ps1

---
+++ Windows/MoarGit/MoarGit.ps1
@@ -0,0 +1,315 @@
+# Script v0.3 │ https://github.com/ettodrzz
+
+# ┌──────────┐
+# │ Contents │
+# └──────────┘
+
+# [Variables] ........................... 30
+
+# [Functions]
+# ├─ Directories 
+# │  └┬─ Check-Directories .............. 44
+# │   └─ Delete-Directories ............. 52
+# ├─ Paths
+# │  └┬─ Check-Paths .................... 61
+# │   ├─ Add-Paths ...................... 70
+# │   └─ Remove-Paths ................... 77
+# ├─ Versions
+# │  └┬─ Print-LocalVersions ............ 87
+# │   └─ Repositories
+# │      └┬─ Get-GitRepoVersion ......... 97
+# │       └─ Get-MoarRepoVersion ........ 109
+# ├─ Downloads
+# │  └┬─ Download-Git ................... 126
+# │   └─ Download-Moar .................. 135
+# └─ Prompt-Continue .................... 144
+
+# [Execution] ........................... 168
+
+# ┌───────────┐
+# │ Variables │
+# └───────────┘
+
+$YellowSign = "$([char]0x1b)[93;49m$([char]0x2022)$([char]0x1b)[0m"
+$GreenSign = "$([char]0x1b)[92;49m$([char]0x2022)$([char]0x1b)[0m"
+$RedSign = "$([char]0x1b)[91;49m$([char]0x2022)$([char]0x1b)[0m"
+
+# ┌───────────┐
+# │ Functions │
+# └───────────┘
+
+# ┌ Directories ┐
+# └─────────────┘
+# ┌─ Checks if the Git and Moar folders already exist in %LocalAppData%\Programs.
+Function Check-Directories
+{
+    if ((Test-Path -Path "$Env:LocalAppData\Programs\Git") -and (Test-Path -Path "$Env:LocalAppData\Programs\Moar"))
+    {
+        return $True
+    }
+}
+# ┌─ Deletes the Git and Moar folders in %LocalAppData%\Programs.
+Function Delete-Directories
+{
+    Remove-Item -Path "$Env:LocalAppData\Programs\Git" -Recurse -Force
+    Remove-Item -Path "$Env:LocalAppData\Programs\Moar" -Recurse -Force
+}
+
+# ┌ Paths ┐
+# └───────┘
+# ┌─ Checks if Git and Moar paths already exist in the current user's Path environment variable.
+Function Check-Paths
+{
+    $PathEnvVar = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+    if (($PathEnvVar -match [regex]::Escape("$Env:LocalAppData\Programs\Git\mingw64\bin")) -and ($PathEnvVar -match [regex]::Escape("$Env:LocalAppData\Programs\Moar")))
+    {
+        return $True
+    }
+}
+# ┌─ Adds Git and Moar paths to the current user's Path environment variable.
+Function Add-Paths
+{
+    $PathEnvVar = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+    $UpdatedPathEnvVar = $PathEnvVar + "%LocalAppData%\Programs\Git\mingw64\bin;%LocalAppData%\Programs\Moar;"
+    [Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Environment", "Path", $UpdatedPathEnvVar, [Microsoft.Win32.RegistryValueKind]::ExpandString)
+}
+# ┌─  Removes Git and Moar paths in the current user's Path environment variable.
+Function Remove-Paths
+{
+    $PathEnvVar = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+    $UpdatedPathEnvVar = (($PathEnvVar).Replace("$Env:LocalAppData\Programs\Git\mingw64\bin;", "")).Replace("$Env:LocalAppData\Programs\Moar;", "")
+    [Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Environment", "Path", $UpdatedPathEnvVar, [Microsoft.Win32.RegistryValueKind]::ExpandString)
+}
+
+# ┌ Versions ┐
+# └──────────┘
+# ┌─ Prints the names of the installed versions of Git and Moar.
+Function Print-LocalVersions
+{
+    Write-Host ""
+    Write-Host "Installed versions:"
+    git --version
+    Write-Host "moar $(moar --version)"
+}
+# ┌ Repository versions ┐
+# └─────────────────────┘
+# ┌─ Gets the name of the latest version of Git in its repository.
+Function Get-GitRepoVersion
+{
+    foreach ($GitLatestTag in (Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/latest" -UseBasicParsing).Links.Href)
+    {
+        if ($GitLatestTag -match "/releases/tag")
+        {
+            return $GitLatestTag -replace "/git-for-windows/git/releases/tag/v", ""
+            break
+        }
+    }
+}
+# ┌─ Gets the name of the latest version of Moar in its repository.
+Function Get-MoarRepoVersion
+{
+    foreach ($MoarLatestTag in (Invoke-WebRequest -Uri "https://github.com/walles/moar/releases/latest" -UseBasicParsing).Links.Href)
+    {
+        if ($MoarLatestTag -match "/releases/tag")
+        {
+            return $MoarLatestTag -replace "/walles/moar/releases/tag/v", ""
+            break
+        }
+    }
+}
+
+# ┌ Downloads ┐
+# └───────────┘
+# ┌─ Downloads the latest version of Git from its repository.
+# │  Saves MinGit.zip in %Temp%, then unzips it to %LocalAppData%\Programs\Git.
+# │  Restores gitconfig file: So that, Notepad is the editor, Moar is the pager, and "main" is the name of the initial branch.
+Function Download-Git
+{
+    $GitRepoVersion = Get-GitRepoVersion
+    Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v$GitRepoVersion/MinGit-$($GitRepoVersion -replace '.windows', '')-64-bit.zip" -OutFile "$Env:Temp\MinGit.zip"
+    Expand-Archive -Path "$Env:Temp\MinGit.zip" -DestinationPath "$Env:LocalAppData\Programs\Git" -Force
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ettodrzz/Cara/main/Windows/MoarGit/gitconfig" -OutFile "$Env:LocalAppData\Programs\Git\etc\gitconfig"
+}
+# ┌─ Downloads the latest version of Moar from its repository.
+# │  Saves moar.exe and its license in %LocalAppData%\Programs\Moar.
+Function Download-Moar
+{
+    $MoarRepoVersion = Get-MoarRepoVersion
+    New-Item -Path "$Env:LocalAppData\Programs\Moar" -ItemType "Directory" -Force | Out-Null
+    Invoke-WebRequest -Uri "https://github.com/walles/moar/releases/download/v$MoarRepoVersion/moar-v$MoarRepoVersion-windows-amd64.exe" -OutFile "$Env:LocalAppData\Programs\Moar\moar.exe"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/walles/moar/master/LICENSE" -OutFile "$Env:LocalAppData\Programs\Moar\LICENSE.txt"
+}
+
+# ┌─ Asks the user if they want to continue with the script.
+Function Prompt-Continue
+{
+    Write-Host "[Y]Yes " -ForegroundColor Yellow -NoNewLine
+    Write-Host "[N]No: " -NoNewLine
+    do
+    {
+        $PromptOption = Read-Host
+        if (($PromptOption -eq "") -or ($PromptOption -eq "Y") -or ($PromptOption -eq "Yes"))
+        {
+            return $True
+        }
+        elseif (($PromptOption -eq "N") -or ($PromptOption -eq "No"))
+        {
+            return $False
+        }
+        else
+        {
+            Write-Host "$RedSign" "Retype a valid option: " -NoNewLine
+        }
+    } while ($PromptOption -notin @("", "Y", "Yes", "N", "No"))
+}
+
+# ┌───────────┐
+# │ Execution │
+# └───────────┘
+
+if (($Args.Count -eq 1) -and (($Args[0] -eq "Remove") -or ($Args[0] -eq "R")))
+{
+    $Directories = Check-Directories
+    $Paths = Check-Paths
+    if ($Directories -and $Paths)
+    {
+        Write-Host "$GreenSign" "Installation found."
+        Print-LocalVersions
+        Write-Host ""
+        Write-Host "Uninstall?"
+        $PromptAnswer = Prompt-Continue
+        if ($PromptAnswer -eq $True)
+        {
+            try
+            {
+                Write-Host ""
+                Write-Host "Deleting directories..."
+                Delete-Directories
+                Write-Host "Removing paths..."
+                Remove-Paths
+                Write-Host "Refreshing paths in the current shell..."
+                $Env:Path =
+                    [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine) +
+                    [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+                Write-Host "$GreenSign" "Done."
+                Write-Host "$YellowSign" "It's necessary to restart other applications so that they stop using the Git and Moar paths, E.g. Visual Studio Code."
+            }
+            catch
+            {
+                Write-Host ""
+                Write-Host "$RedSign" "Something went wrong. Visit https://github.com/ettodrzz/Cara to see how to uninstall manually."
+            }
+        }
+        else
+        {
+            Write-Host ""
+            Write-Host "$YellowSign" "Uninstall canceled."
+        }
+    }
+    else
+    {
+       Write-Host "$RedSign" "Installation not found."
+    }
+
+}
+elseif ($Args.Count -eq 0)
+{
+    $Directories = Check-Directories
+    $Paths = Check-Paths
+    if ($Directories -and $Paths)
+    {
+        Write-Host "Checking for updates..."
+        $GitRepoVersion = Get-GitRepoVersion
+        $MoarRepoVersion = Get-MoarRepoVersion
+        switch ($True)
+        {
+            ((git --version) -ne "git version $GitRepoVersion") {Write-Host "$YellowSign" "Git update found ($GitRepoVersion)."; $GitUpdateAvailable = $True}
+            ((moar --version) -ne "v$MoarRepoVersion") {Write-Host "$YellowSign" "Moar update found ($MoarRepoVersion)."; $MoarUpdateAvailable = $True}
+            Default {Write-Host "$GreenSign" "Git and Moar are up to date."}
+        }
+        if ($GitUpdateAvailable -or $MoarUpdateAvailable)
+        {
+            Write-Host ""
+            Write-Host "Update?"
+            $PromptAnswer = Prompt-Continue
+            if ($PromptAnswer -eq $True)
+            {
+                try
+                {
+                    Write-Host ""
+                    Write-Host "Updating..."
+                    if ($GitUpdateAvailable)
+                    {
+                        Write-Host "Downloading Git..."
+                        Download-Git
+                    }
+                    if ($MoarUpdateAvailable)
+                    {
+                        Write-Host "Downloading Moar..."
+                        Download-Moar
+                    }
+                    Write-Host "$GreenSign" "Done."
+                    Print-LocalVersions
+                }
+                catch
+                {
+                    Write-Host ""
+                    Write-Host "$RedSign" "Something went wrong. Visit https://github.com/ettodrzz/Cara to see how to update manually."
+                    Print-LocalVersions
+                }
+            }
+            else
+            {
+                Write-Host ""
+                Write-Host "$YellowSign" "Update not started."
+                Print-LocalVersions
+            }
+        }
+        else
+        {
+            Print-LocalVersions
+        }
+    }
+    else
+    {
+        $GitRepoVersion = Get-GitRepoVersion
+        $MoarRepoVersion = Get-MoarRepoVersion
+        Write-Host "$YellowSign" "Git ($GitRepoVersion) and Moar ($MoarRepoVersion) will be installed."
+        Write-Host ""
+        Write-Host "Install?"
+        $PromptAnswer = Prompt-Continue
+        if ($PromptAnswer -eq $True)
+        {
+            try
+            {
+                Write-Host ""
+                Write-Host "Downloading Git..."
+                Download-Git
+                Write-Host "Downloading Moar..."
+                Download-Moar
+                Write-Host "Adding paths..."
+                Add-Paths
+                Write-Host "Refreshing paths in the current shell..."
+                $Env:Path =
+                    [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine) +
+                    [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+                Write-Host "$GreenSign" "Done."
+                Write-Host "$YellowSign" "Other apps need to be restarted to start using Git and Moar, E.g. Visual Studio Code."
+            }
+            catch
+            {
+                Write-Host ""
+                Write-Host "$RedSign" "Something went wrong. Visit https://github.com/ettodrzz/Cara to see how to install manually."
+            }
+        }
+        else
+        {
+            Write-Host ""
+            Write-Host "$YellowSign" "Installation canceled."
+        }
+    }
+}
+else
+{
+    Write-Host "$RedSign" "Argument not valid."
+}
