ab3f5f4ebdb15445b6b1dd9d4318714d339c07f9
Hector Pedraza <ettodrzz@gmail.com>
2024-02-07 14:08:15 -0600

    update bash_prompt.sh

--- windows/wsldeb/scripts/bash_prompt.sh
+++ windows/wsldeb/scripts/bash_prompt.sh
@@ -1,28 +1,18 @@
 custom_prompt()
 {
-	# Generate random RGB color
+	# Prompt color
 	additive()
 	{
 		echo "$(shuf --input-range 200-250 --head-count 1)"
 	}
-	local color="$(additive);$(additive);$(additive)"
-
-	# Prompt colors
-	local color_bg="\[\e[38;2;0;0;0;48;2;${color}m\]"
-	local color_fg="\[\e[38;2;${color};49m\]"
+	local color="\[\e[38;2;0;0;0;48;2;$(additive);$(additive);$(additive)m\]"
 	local color_reset="\[\e[0m\]"
-
-	# Nerd Fonts
-	# Name                 | Logo | Code
-	# ---------------------|------|-----
-	# pl-left_hard_divider |     | e0b0
-	local div="$(echo -e "\Ue0b0")"
 
 	# Change window title
 	PS1="\[\033]2;\w\007\]"
 
 	# Change prompt
-	PS1+="$color_bg \$ $color_fg$div$color_reset "
+	PS1+="$color \$ $color_reset "
 
 	unset additive
 }
